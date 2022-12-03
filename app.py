96069e373b5ef8bbd7b019fd5758e48bbb058d27
Sarah George <sngeorge27@gmail.com>
2022-12-02 22:26:19 -0500

    login page creation

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, request, jsonify, render_template, url_for
+from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello World'
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('dashboard'))
+    return render_template('login.html', error=error)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5400, debug=True)
-
+    app.run(debug=True)
